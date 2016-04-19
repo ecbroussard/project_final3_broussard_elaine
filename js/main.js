dacc83a292a825dfc2ce1fa6316045c6ccfc22b9
Elaine Broussard <elaine@elainebroussard.com>
2016-04-19 15:17:39 -0500

    add custom javascript code to show/hide content corresponding to nav links

--- js/main.js
+++ js/main.js
@@ -31,4 +31,32 @@ $( document ).ready(function() {
 			$(this).prop("src", "img/contactinactive.png");
 		}
 	);
+	document.getElementById( 'specials').onclick = function(event) {
+		var contentdivs = document.getElementsByClassName( 'maincontent' );
+		for ( var i=0; i < contentdivs.length; i++) {
+			contentdivs[i].style.display = 'none';
+		}
+		document.getElementById('specialscontent').style.display='block';
+	}
+	document.getElementById( 'events').onclick = function(event) {
+		var contentdivs = document.getElementsByClassName( 'maincontent' );
+		for ( var i=0; i < contentdivs.length; i++) {
+			contentdivs[i].style.display = 'none';
+		}
+		document.getElementById('eventscontent').style.display='block';
+	}
+	document.getElementById( 'contact').onclick = function(event) {
+		var contentdivs = document.getElementsByClassName( 'maincontent' );
+		for ( var i=0; i < contentdivs.length; i++) {
+			contentdivs[i].style.display = 'none';
+		}
+		document.getElementById('contactcontent').style.display='block';
+	}
+	document.getElementById( 'menu').onclick = function(event) {
+		var contentdivs = document.getElementsByClassName( 'maincontent' );
+		for ( var i=0; i < contentdivs.length; i++) {
+			contentdivs[i].style.display = 'none';
+		}
+		document.getElementById('menucontent').style.display='block';
+	}
 });
